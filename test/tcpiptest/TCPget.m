4a6eb5df495fbf6809a48864b6002b8c695b33e7
Oliver Hinds <oph@mit.edu>
2008-02-12 06:04:44 +0000

    working!!
    git-svn-id: file:///repos/festr/trunk@87 9051720a-062f-4424-aca9-25ff1f032159

--- test/tcpiptest/TCPget.m
+++ test/tcpiptest/TCPget.m
@@ -1,17 +1,32 @@
-function [strOut] =  TCPget(hostname,port)
+function [strOut] =  TCPget(hostname,port,msg)
 
+import java.*;
 import java.io.*;
 import java.net.*;
 import java.lang.*;
 
+% msg termination string
+term = String(sprintf('\n'));
+
 host = String(hostname);
-%port = 15003;
 
 skt =  Socket(host,port);
 in =  BufferedReader(InputStreamReader(skt.getInputStream()));
-tmp = in.readLine();
-strOut = char(tmp);
+out =  PrintWriter(skt.getOutputStream(),true);
+
+% send a request
+xmlstr = String('<?xml version="1.0" encoding="UTF-8"?><info><roi id="roi_id"><get name="activation" trStart="120" trEnd="140"></get></roi></info>\n');
+
+out.println(xmlstr);
+
+strOut = [];
+tmp = 'a';
+while(~strcmp(tmp,term) && tmp ~= -1)
+  tmp = in.read();
+  strOut(end+1) = char(tmp);
+end
 disp(['Retrieved from server socket: ' strOut]);
 
 in.close();
+out.close();
 skt.close();

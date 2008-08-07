90c574dee95b5bd696056e013743852872b5dc86
Oliver Hinds <oph@mit.edu>
2008-08-07 15:19:17 +0000

    added
    git-svn-id: file:///repos/festr/trunk@156 9051720a-062f-4424-aca9-25ff1f032159

---
+++ src/stream/RtProcessor.cpp
@@ -0,0 +1,87 @@
+/******************************************************************************
+ * RtProcessor.cpp is the definition of a class for a processing stream
+ *
+ * Oliver Hinds <ohinds@mit.edu> 2007-08-14 
+ * 
+ *****************************************************************************/
+
+static char *VERSION = "$Id: RtProcessor.cpp 107 2008-04-18 02:00:17Z ohinds $";
+
+#include"RtProcessor.h"
+#include"RtDataIDs.h"
+#include"RtDiff.h"
+#include"RtVar.h"
+
+#include"tinyxml/tinyxml.h"
+
+// default constructor
+RtProcessor::RtProcessor() 
+  : RtStreamComponent() {
+  setID("RtProcessor");
+}
+
+// destructor
+RtProcessor::~RtProcessor() {
+  
+}
+
+//*** initialization routines  ***//
+
+// add modules based on the processor configuration
+//  in
+//   config
+//  out
+//   success or failure
+int RtProcessor::addModules(RtConfig &config) {
+  ACE_TRACE(("RtProcessor::addModules"));
+
+  // make sure the stack is empty
+  while(!addMod.empty()) {
+    addMod.pop();
+  }
+
+  // get the node for preprocessing 
+  TiXmlNode *preprocNode = config.getNode("processor");
+
+  // if no preprocessing was specified, just pass the data
+  if(preprocNode == NULL || preprocNode->Type() != TiXmlNode::ELEMENT) {
+    addSingleModule(RtPasser::moduleString, "original data passer")
+      ->addOutput(config.getConductor()->getDisplay());
+  }
+  else { // find modules in the node and add each
+    addModulesFromNode((TiXmlElement*) preprocNode, &config);
+  }
+    // all modules are now added to the stream
+  pushAllModules();
+
+
+  return 0;
+}
+
+// process a single acquisition
+int RtProcessor::process(ACE_Message_Block *mb) {
+  ACE_TRACE(("RtProcessor::process"));
+
+  // pass the message block down to our stream
+  RtStream::put(mb);  
+
+  return 0;
+}
+
+// get the version
+//  out: char array that represents the cvs version
+char *RtProcessor::getVersionString() {
+  return VERSION;
+}
+
+
+/*****************************************************************************
+ * $Source$
+ * Local Variables:
+ * mode: c++
+ * fill-column: 76
+ * comment-column: 0
+ * End:
+ *****************************************************************************/
+
+
---
+++ src/stream/RtProcessor.h
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * RtProcessor.h is the header for a class for a processing stream
+ * component for a real-time fMRI.
+ *
+ * Oliver Hinds <ohinds@mit.edu> 2007-08-14 
+ * 
+ *****************************************************************************/
+
+#ifndef RTPROCESSOR_H
+#define RTPROCESSOR_H
+
+#include"RtStreamComponent.h"
+#include"RtData.h"
+#include"RtPasser.h"
+
+// class declaration
+class RtProcessor : public RtStreamComponent, public RtStream {
+
+public:
+
+  //*** constructors/destructors  ***//
+  
+  // default constructor
+  RtProcessor();
+
+  // destructor
+  ~RtProcessor();
+
+  //*** initialization routines  ***//
+
+
+  // adds all modules to the stream
+  //  in
+  //   config: configuration info
+  int addModules(RtConfig &config);
+
+  // get the version
+  //  out: char array that represents the cvs version
+  virtual char *getVersionString();
+
+  
+
+protected:
+
+  // process a single acquisition
+  int process(ACE_Message_Block *mb);
+
+};
+
+#endif
+
+/*****************************************************************************
+ * $Source$
+ * Local Variables:
+ * mode: c++
+ * fill-column: 76
+ * comment-column: 0
+ * End:
+ *****************************************************************************/
+
+

cd27deec01c80893395731a03ef686f54fa0a00f
Oliver Hinds <oph@mit.edu>
2008-08-19 05:00:53 +0000

    added
    git-svn-id: file:///repos/festr/trunk@185 9051720a-062f-4424-aca9-25ff1f032159

---
+++ src/executive/debug_levels.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * debug levels
+ *
+ * Oliver Hinds <ohinds@mit.edu> 2008-08-19 
+ *****************************************************************************/
+
+#ifndef	DEBUG_LEVELS_H
+#define DEBUG_LEVELS_H
+
+#define TEMP     2
+#define BASIC    4
+#define MODERATE 8
+#define ALLOC    16
+#define ADVANCED 32
+
+#define DEBUG_LEVEL 0
+
+#endif

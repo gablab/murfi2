26d8e2c5d635f01e1c544de46b65a96198b3bc0f
Alexey Sidorov <AlexSidorov@unknown.com>
2008-09-19 17:07:34 +0000

    delete unused stuff
    git-svn-id: file:///repos/festr/trunk@236 9051720a-062f-4424-aca9-25ff1f032159

--- src/frontend/FrToolsPanel/FrToolsPanel.cpp
+++
@@ -1,7 +0,0 @@
-#include "FrToolsPanel.h"
-
-FrToolsPanel::FrToolsPanel(QWidget *parent): QWidget(parent){
-	setupUi(this);
-
-//	toolBox->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
-}

92758099df40fa7b22edaf94aa03ac8ecd6ab13e
Alexey Sidorov <AlexSidorov@unknown.com>
2008-10-24 14:46:53 +0000

    + base layer
    git-svn-id: file:///repos/festr/trunk@288 9051720a-062f-4424-aca9-25ff1f032159

---
+++ src/frontend/Layers/FrBaseLayer.cpp
@@ -0,0 +1,51 @@
+#include "FrBaseLayer.h"
+#include "FrColormapFilter.h"
+#include "FrTBCFilter.h"
+#include "FrSettings.h"
+#include "FrMacro.h"
+
+// VTK stuff
+#include "vtkObjectFactory.h"
+#include "vtkImageData.h"
+#include "vtkImageActor.h"
+#include "vtkRenderer.h"
+#include "vtkCamera.h"
+
+// Some defines
+#define DEF_REN_BACKGROUND 0.0, 0.0, 0.0
+
+vtkStandardNewMacro(FrBaseLayer);
+
+FrBaseLayer::FrBaseLayer() 
+: m_Renderer(0) {
+    // renderer
+    m_Renderer = vtkRenderer::New();
+    m_Renderer->SetBackground(DEF_REN_BACKGROUND);
+    m_Renderer->GetActiveCamera()->ParallelProjectionOn();
+}
+
+FrBaseLayer::~FrBaseLayer(){        
+    if(m_Renderer){
+        m_Renderer->Delete();
+    }
+}
+
+void FrBaseLayer::SetCameraSettings(FrCameraSettings& settings){
+    if(!m_Renderer) return;
+
+    vtkCamera* cam = m_Renderer->GetActiveCamera();
+    cam->SetPosition(settings.Position);
+    cam->SetFocalPoint(settings.FocalPoint);
+    cam->SetViewUp(settings.ViewUp);
+    cam->SetParallelScale(settings.Scale);
+}
+
+void FrBaseLayer::GetCameraSettings(FrCameraSettings& settings){
+    if(!m_Renderer) return;
+    
+    vtkCamera* cam = m_Renderer->GetActiveCamera();
+    COPY_ARR3(settings.Position, cam->GetPosition());
+    COPY_ARR3(settings.FocalPoint, cam->GetFocalPoint());
+    COPY_ARR3(settings.ViewUp, cam->GetViewUp());
+    settings.Scale = cam->GetParallelScale();
+}
---
+++ src/frontend/Layers/FrBaseLayer.h
@@ -0,0 +1,40 @@
+// class combining renderer and actors for single layer display
+#ifndef FR_BASE_LAYER
+#define FR_BASE_LAYER
+
+//Forward declarations
+class vtkRenderer;
+class FrCameraSettings;
+
+// Some includes
+#include "vtkObject.h"
+#include "FrMacro.h"
+
+// Represents layer object
+class FrBaseLayer : public vtkObject {
+public:
+    static FrBaseLayer* New();
+
+    // Properties
+    FrPropMacro(int, ID);
+
+    void SetCameraSettings(FrCameraSettings& settings);
+    void GetCameraSettings(FrCameraSettings& settings);
+
+    // Accessors/Modifiers
+    vtkRenderer* GetRenderer(){ return m_Renderer; }
+    
+protected:
+    vtkRenderer* m_Renderer;
+
+private:
+    FrBaseLayer(const FrBaseLayer&);  // Not implemented.
+    void operator=(const FrBaseLayer&);  // Not implemented.
+
+protected:
+    // Constructor and destructor
+    FrBaseLayer();
+    virtual ~FrBaseLayer();
+};
+
+#endif

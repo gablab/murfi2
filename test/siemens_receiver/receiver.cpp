e8385a9af1af8cdb8c1d2200737c561131bc6722
Oliver Hinds <oph@mit.edu>
2009-05-31 14:36:07 +0000

    added
    git-svn-id: file:///repos/festr/trunk@492 9051720a-062f-4424-aca9-25ff1f032159

---
+++ test/siemens_receiver/receiver.cpp
@@ -0,0 +1,7 @@
+/******************************************************************************
+ * program to save images sent from the scanner
+ *
+ * Oliver Hinds <ohinds@mit.edu> 2009-05-31
+ *****************************************************************************/
+
+#include"ExternalSenderImageInfo.h"

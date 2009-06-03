2ee300478930b132e278c192d60c6dcd87e26a07
Oliver Hinds <oph@mit.edu>
2009-06-03 17:30:18 +0000

    first pass
    git-svn-id: file:///repos/festr/trunk@494 9051720a-062f-4424-aca9-25ff1f032159

--- test/siemens_receiver/receiver.cpp
+++ test/siemens_receiver/receiver.cpp
@@ -1,5 +1,6 @@
 /******************************************************************************
- * program to save images sent from the scanner
+ * program to save images sent from a scanner using the external
+ * sender ice functor
  *
  * Oliver Hinds <ohinds@mit.edu> 2009-05-31
  *****************************************************************************/
@@ -15,16 +16,38 @@
 #include <iostream>
 using namespace std;
 
-const int bufferLen = 1024;
+
+// defaults
+static const unsigned int defaultPort = 15000;
 
 
 // prototypes
-SOCKET SetUpListener(const char* pcAddress, int nPort);
-SOCKET AcceptConnection(SOCKET ListeningSocket, sockaddr_in& sinRemote);
-bool EchoIncomingPackets(SOCKET sd);
+int runReceiverWin(unsigned int port);
+SOCKET getListenerWin(unsigned int port);
+SOCKET acceptWin(SOCKET sock, sockaddr_in& remote);
+ExternalImageInfo *readImageInfoWin(SOCKET sock);
+short *readImageDataWin(SOCKET sock, const ExternalImageInfo &info);
+int closeWin(SOCKET sock);
 
 
-int runWinReceiver(unsigned int port) {
+// controller
+int main(int argc, char** argv) {
+  
+  // get port if specified
+  unsigned int port = defaultPort
+  if(argc > 1) {
+    port = atoi(argv[1]);
+  }
+
+  cout << "trying to start scanner listener on port " << port << endl;
+  runReceiverWin(port);
+  cout << "done" << endl;
+}
+
+
+// listens for incomming images on a port until error
+// out: 0 for sucess, 1 for error
+int runReceiverWin(unsigned int port) {
   cout << "starting the receiver...";
   SOCKET sock = getWinListener(htons(port));
   if(sock == INVALID_SOCKET) {
@@ -46,18 +69,28 @@ int runWinReceiver(unsigned int port) {
       return 1;
     }
 
-    // receive and build image
+    // receive and build header
+    ExternalImageInfo *info = readImageInfo(sd);
+
+    // receive and build data
+    short *data = readImageData(sd,info);
+
+    // error check 
+    if(info == NULL || data == NULL) {
+      cout << "error receiving image, exit" << endl;
+      break;
+    }
     
-
     // close connection
-
+    close(sd);
   }
 
   return 0;
 }
 
-
-SOCKET getWinListener(unsigned int port) {
+// get a new socket on a local port to listen on
+// out: SOCKET on sucess, INVALID_SOCKET on error
+SOCKET getListenerWin(unsigned int port) {
   SOCKET sd = socket(AF_INET, SOCK_STREAM, 0);
   if(sd != INVALID_SOCKET) {
     sockaddr_in sin;
@@ -73,99 +106,86 @@ SOCKET getWinListener(unsigned int port) {
   return INVALID_SOCKET;
 }
 
-
-//// AcceptConnection //////////////////////////////////////////////////
-// Waits for a connection on the given socket.  When one comes in, we
-// return a socket for it.  If an error occurs, we return
-// INVALID_SOCKET.
-
-SOCKET AcceptConnection(SOCKET ListeningSocket, sockaddr_in& sinRemote)
-{
-  int nAddrSize = sizeof(sinRemote);
-  return accept(ListeningSocket, (sockaddr*)&sinRemote, &nAddrSize);
+// accept an incoming connection
+// out: return value of accept()
+SOCKET acceptWin(SOCKET sock, sockaddr_in& remote) {
+  int nAddrSize = sizeof(remote);
+  return accept(sock, (sockaddr*)&remote, &nAddrSize);
 }
 
+// read an image header from the socket
+//  in
+//   sock: a socket to receive on
+//  out
+//   image info struct on successful read (NULL otherwise)
+ExternalImageInfo *readImageInfoWin(SOCKET sock) {
+  char buffer[EXTERNALSENDERSIZEOF];
+  unsigned int rec;
 
-//// EchoIncomingPackets ///////////////////////////////////////////////
-// Bounces any incoming packets back to the client.  We return false
-// on errors, or true if the client closed the socket normally.
+  // read header
+  for(rec = 0; rec < EXTERNALSENDERSIZEOF && rec != SOCKET_ERROR; 
+      rec += recv(sock, buffer+rec, EXTERNALSENDERSIZEOF-rec, 0));
 
-bool EchoIncomingPackets(SOCKET sd)
-{
-  // Read data from client
-  char acReadBuffer[kBufferSize];
-  int nReadBytes;
-  do {
-    nReadBytes = recv(sd, acReadBuffer, kBufferSize, 0);
-    if (nReadBytes > 0) {
-      cout << "Received " << nReadBytes <<
-	" bytes from client." << endl;
+  // error check
+  if(rec != EXTERNALSENDERSIZEOF) {
+    cout << "error receiving header" << endl;
+    return NULL;
+  }
 
-      int nSentBytes = 0;
-      while (nSentBytes < nReadBytes) {
-	int nTemp = send(sd, acReadBuffer + nSentBytes,
-			 nReadBytes - nSentBytes, 0);
-	if (nTemp > 0) {
-	  cout << "Sent " << nTemp <<
-	    " bytes back to client." << endl;
-	  nSentBytes += nTemp;
-	}
-	else if (nTemp == SOCKET_ERROR) {
-	  return false;
-	}
-	else {
-	  // Client closed connection before we could reply to
-	  // all the data it sent, so bomb out early.
-	  cout << "Peer unexpectedly dropped connection!" <<
-	    endl;
-	  return true;
-	}
-      }
-    }
-    else if (nReadBytes == SOCKET_ERROR) {
-      return false;
-    }
-  } while (nReadBytes != 0);
+  cout << "received header" << endl;
+  ExternalImageInfo *info = new ExternalImageInfo(buffer, bytesRead);
 
-  cout << "Connection closed by peer." << endl;
-  return true;
+  return info;
 }
 
+// read an image info from a socket
+// NOTE: performes blocking read
+//  in
+//   stream: a socket stream to receive on
+//   info:   the last read image info struct
+//  out
+//   image data on successful read (NULL otherwise)
+short *readImageDataWin(SOCKET sock, const ExternalImageInfo &info) {
+  cout << "receiving image " << info.iAcquisitionNumber << endl;
 
-bool ShutdownConnection(SOCKET sd)
-{
-    // Disallow any further data sends.  This will tell the other side
-    // that we want to go away now.  If we skip this step, we don't
-    // shut the connection down nicely.
-    if (shutdown(sd, SD_SEND) == SOCKET_ERROR) {
-        return false;
-    }
+  unsigned int numPix 
+    = (int) pow((double)info.iMosaicGridSize,2)*info.nLin*info.nCol;
+  unsigned int rec;
 
-    // Receive any extra data still sitting on the socket.  After all
-    // data is received, this call will block until the remote host
-    // acknowledges the TCP control packet sent by the shutdown above.
-    // Then we'll get a 0 back from recv, signalling that the remote
-    // host has closed its side of the connection.
-    char acReadBuffer[kBufferSize];
-    while (1) {
-        int nNewBytes = recv(sd, acReadBuffer, kBufferSize, 0);
-        if (nNewBytes == SOCKET_ERROR) {
-            return false;
-        }
-        else if (nNewBytes != 0) {
-            cerr << endl << "FYI, received " << nNewBytes <<
-                    " unexpected bytes during shutdown." << endl;
-        }
-        else {
-            // Okay, we're done!
-            break;
-        }
-    }
+  // read image data
+  for(rec = 0; rec < numPix*sizeof(short) && rec != SOCKET_ERROR;
+      rec += recv(sock, buffer+rec, numPix*sizeof(short)-rec));
 
-    // Close the socket.
-    if (closesocket(sd) == SOCKET_ERROR) {
-        return false;
-    }
+  // error check
+  if(rec != numPix*sizeof(short)) {
+    cout << "error receiving image data" << endl;
+    return NULL;
+  }
 
-    return true;
+  short *img = new short[numPix];
+  memcpy(img,buffer,numPix*sizeof(short));
+
+  return img;
+}
+
+// gracefully close a socket
+int closeWin(SOCKET sock) {
+  if(shutdown(sock, SD_SEND) == SOCKET_ERROR) {
+    return 1;
+  }
+
+  // clear remaining data
+  char buffer[1024];
+  unsigned int num = 1;
+  while(0 != (num = recv(sock, buffer, 1024, 0)) && num != SOCKET_ERROR);
+
+  if(num == SOCKET_ERROR) {
+    return 1;
+  }
+
+  if(closesocket(sock) == SOCKET_ERROR) {
+    return 1;
+  }
+
+  return 0;
 }

7f9b977dfc964d7d7e4e1ebbfa753326009be455
Satrajit Ghosh <satra@mit.edu>
2012-07-08 11:26:24 -0400

    rf: changed makefile to create separate c++ matlab and python examples

--- util/infoclient/test_infoclient.py
+++ util/infoclient/test_infoclient.py
@@ -9,6 +9,9 @@ ic = InfoClient()
 ic.start(15003, 'localhost', 15002)
 ic.add('activation-img', 'active')
 
+sleep(3)
+# at present this only prints how many elements are in the info vector
+ic.check()
 
 '''
 ic.send('test message', 'localhost', 15002)

faa271d97cc4a0ba9f13b9ee9cf345898a162849
ohinds <ohinds@curtis.(none)>
2013-02-19 12:57:40 -0500

    Substantial progress on a regression test.

--- test/regression/neurofeedback.py
+++ test/regression/neurofeedback.py
@@ -2,6 +2,7 @@
 
 import multiprocessing
 import os
+import re
 import subprocess
 import sys
 
@@ -9,9 +10,10 @@ def run(v):
 
     cmd = v[0]
     cwd = v[1]
-    proc = subprocess.Popen(cmd, cwd=cwd)
+    proc = subprocess.Popen(cmd, cwd=cwd,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
-    return proc.returncode
+    return out
 
 
 def main():
@@ -25,11 +27,26 @@ def main():
     runs += [[["../bin/murfi",
                "-f" "scripts/neurofeedback_nodisplay.xml"],
               "../../example_data"]]
-    runs += [[["./servedata.sh", "200", "15000", "localhost", "2"],
+    runs += [[["./servedata.sh", "100", "15000", "localhost", "2"],
               "../../example_data/scripts"]]
 
     pool = multiprocessing.Pool(2)
-    pool.map(run, runs)
+    out = pool.map(run, runs)
+
+    # get results by TR
+    tr_data = []
+    tr_searcher = re.compile(
+        r"RtRoiCombine: active at tr (\d+) result is: ([-\d.]+)")
+    for line in out[0].split("\n"):
+        tr_match = tr_searcher.search(line)
+        if tr_match is not None:
+            tr = int(tr_match.groups()[0]) - 1
+            assert tr == len(tr_data), "out of order tr outputs: %d != %d" % (
+                tr, len(tr_data))
+            tr_data.append(float(tr_match.groups()[1]))
+
+    # read the regression standard data
+
 
 if __name__ == "__main__":
     main()

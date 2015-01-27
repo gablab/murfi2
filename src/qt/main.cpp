87fab2f1db38a44b4e4e7d6a9e2dbeb4bf4d85a1
ohinds <ohinds@gmail.com>
2015-01-27 14:55:56 -0500

    File dialog to choose the subject directory, if it's not exported as an env var.

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -1,10 +1,51 @@
+#include <cstdlib>
+#include <iostream>
+
 #include "MainWindow.h"
 #include <QApplication>
+#include <QFileDialog>
+#include <QInputDialog>
 
 #include "RtExperiment.h"
 
+using std::cout;
+using std::cerr;
+using std::endl;
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    if (getenv("MURFI_SUBJECTS_DIR") == NULL ||
+        getenv("MURFI_SUBJECT_NAME") == NULL) {
+
+
+      QString dir = QFileDialog::getExistingDirectory(
+        NULL, "Subject directory", getenv("MURFI_SUBJECTS_DIR"),
+        QFileDialog::ShowDirsOnly);
+
+      if (!dir.isEmpty()) {
+        string chosen = dir.toStdString();
+        size_t delim = chosen.rfind('/');
+        if (delim == string::npos || delim == 0) {
+          cerr << "failed to parse subject directory" << endl;
+          return 1;
+        }
+
+        string subjs_dir = chosen.substr(0, delim);
+        string subj = chosen.substr(delim + 1);
+
+        cout << "Setting MURFI_SUBJECTS_DIR=" << subjs_dir << endl;
+        setenv("MURFI_SUBJECTS_DIR", subjs_dir.c_str(), 1);
+
+        cout << "Setting MURFI_SUBJECT_NAME=" << subj << endl;
+        setenv("MURFI_SUBJECT_NAME", subj.c_str(), 1);
+      }
+      else {
+        cerr << "Subjects directory must be specified" << endl;
+        return 1;
+      }
+
+    }
 
     if (!initializeSystem(argc, argv)) {
       return 1;

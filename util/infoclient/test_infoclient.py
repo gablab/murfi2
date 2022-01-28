58d9a80ca99a28f66bfe72a86614b6a3ba875972
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-29 03:40:51 +0800

    Delete util directory

--- util/infoclient/test_infoclient.py
+++
@@ -1,6 +0,0 @@
-
-from infoclientLib import InfoClient
-ic = InfoClient('localhost', 15002, 'localhost', 15003)
-ic.add('roi-weightedave', 'active')
-ic.start()
-
--- util/infoserver/getRTUpdate.m
+++
@@ -1,7 +0,0 @@
-function response = getRTUpdate(strParams)
-
-% get response
-sIncXML = tcpipInfoGet(strParams);
-
-% parse response from the infoserver
-response = parseInfoServerResponse(sIncXML);
